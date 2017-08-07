314313f6bf6fe38b5b7bcda6587f67d3887b3e0f
ayushlabh <labhayush14@gmail.com>
2017-08-07 12:57:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$title = "Home";
+$content = '
+        <img src="Images/coffee1.png" class="imgLeft" />
+        <h3>COLD BREW COFFEE</h3>
+        <p>
+            Cold brew, also called cold water extract or cold press, is the process of steeping coffee
+            grounds in water at cool temperatures for an extended period. Coarse-ground beans are soaked
+            in water for a prolonged period of time, usually 12 hours or more. The water is normally 
+            kept at room temperature, but chilled water is also used. The grounds are filtered out of 
+            the water after they have been steeped using a paper coffee filter, a fine metal sieve, a 
+            rench press or felt, in the case of the "Toddy" brewing system. The result is a coffee 
+            concentrate that is often diluted with water or milk, and is served hot, over ice, or blended
+            with ice and chocolate. 
+
+        <img src="Images/Cappuccino.jpeg" class="imgRight" />
+        <h3>CAPPUCCINO</h3>
+        <p>
+            Cappuccino is a coffee-based drink prepared with espresso, hot milk, and steamed milk foam. A
+            cappuccino differs from a caffè latte in that it is prepared with much less steamed or textured
+            milk than the caffè latte with the total of espresso and milk/foam making up between approximately
+            150 and 180 millilitres (5 and 6 US fluid ounces). A cappuccino usually exceeds the height of the 
+            cup, making the foam visible above the side of the cup. A cappuccino is traditionally served in a
+            porcelain cup, which has far better heat retention characteristics than glass or paper. The foam 
+            on top of the cappuccino acts as an insulator and helps retain the heat of the liquid, allowing it
+            to stay hotter longer.
+         </p>
+
+         <img src="Images/Latte.jpg" class="imgLeft" />
+         <h3>LATTE</h3>
+         <p>
+            A latte is an espresso and steamed milk,[50][51] generally in a 1:3 to 1:5 ratio of espresso to milk,
+            with a little foam on top.In Italy it is called caffè latte or caffelatte, which means "coffee and milk".
+            In northern Europe and Scandinavia the term café au lait has traditionally been used for the combination
+            of espresso and milk, but this term is used in the US for brewed coffee and scalded milk. In France,
+            caffè latte is mostly known from American coffee chains; a combination of espresso and steamed milk
+            equivalent to a latte is in French called grand crème and in German Milchkaffee or Melange. In
+            Portuguese it is called galão.
+
+
+         </p>';
+include 'Template.php';
+?>
